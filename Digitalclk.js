b47abbf6078af9ab69fe19eaf95e561b98780f05
chandana2341 <134035761+chandana2341@users.noreply.github.com>
2023-05-26 22:22:15 +0530

    Add files via upload

---
+++ Digitalclk.js
@@ -0,0 +1,219 @@
+function clk()
+{
+    var date=new Date()
+    var hh=date.getHours()
+    var mm=date.getMinutes()
+    var ss=date.getSeconds()
+    var dd=date.getDate()
+    var mo=date.getMonth()
+    var yy=date.getFullYear() 
+    var dy=date.getDay()
+    var am_pm="AM"
+    if(hh>=12)
+    {
+        am_pm="PM"
+        if(hh>=13)
+        {
+        hh-=12
+        }
+    }
+    if(hh==0)
+    {
+        hh=12
+    }
+    var fulltime=`${hh}:${mm}${am_pm}`
+    document.getElementById("Time").innerHTML=fulltime
+    document.getElementById("Seconds").innerHTML=ss
+    if(mo==0)
+    {
+        mon="Jan"
+    }
+    else if(mo==1)
+    {
+        mo="Feb"
+    }
+    else if(mo==2)
+    {
+        mo="Mar"
+    }
+    else if(mo==3)
+    {
+        mo="Apr"
+    }
+    else if(mo==4)
+    {
+        mo="May"
+    }
+    else if(mo==5)
+    {
+        mo="Jun"
+    }
+    else if(mo==6)
+    {
+        mo="Jul"
+    }
+    else if(mo==7)
+    {
+        mo="Aug"
+    }
+    else if(mo==8)
+    {
+        mo="Sep"
+    }
+    else if(mo==9)
+    {
+        mo="Oct"
+    }
+    else if(mo==10)
+    {
+        mo="Nov"
+    }
+    else if(mo==11)
+    {
+        mo="Dec"
+    }
+    var fullDate=`${dd}/${mo}/${yy}`
+    var bdy=document.querySelector("body")
+    document.getElementById("Date").innerHTML=fullDate
+    switch(dy)
+    {
+       
+        case 0:dy="Sunday"
+        bdy.style.backgroundImage="url(sun.jpg)"
+       
+        break;
+        case 1:dy="Monday"
+        bdy.style.backgroundImage="url(monImg.jpg)"
+       
+        break;
+        case 2:dy="Tuesday"
+        bdy.style.backgroundImage="url(tueImg.jpg)"
+        
+        break;
+        case 3:dy="Wednesday"
+        bdy.style.backgroundImage="url(wedimg.jpg)"
+       
+        break;
+        case 4:dy="Thursday"
+        bdy.style.backgroundImage="url(thurimg.jpg)"
+       
+        break;
+        case 5:dy="Friday"
+        bdy.style.backgroundImage="url(frimage.jpg)"
+       
+        break;
+
+        case 6:dy="Saturday"
+        bdy.style.backgroundImage="url(sat.jpg)"
+        break;
+    }
+    document.getElementById("Day").innerHTML=dy
+    setTimeout(clk,1000)
+}
+
+function greet()
+{
+    var date=new Date()
+    var time=date.getHours()
+    if(time>=5 && time<=12)
+    {
+        document.getElementById("message").innerHTML="Good Morning!!"
+        document.getElementById("vid").src="./morning.mp4"
+        document.getElementById("container").style.backgroundColor=" rgba(200, 217, 224, 0.548)"
+        document.getElementById("Time").style.color="black"
+        document.getElementById("Date").style.color="darkblue"
+        document.getElementById("Day").style.color="darkblue"
+        document.getElementById("Seconds").style.color="white"
+        
+        
+    }
+    else if(time>=12 && time<16)
+    {
+        document.getElementById("message").innerHTML="Good Afternoon!!"
+        document.getElementById("vid").src="./afternoon.mp4"
+        document.getElementById("Time").style.color="black"
+        document.getElementById("Date").style.color="darkblue"
+        document.getElementById("Day").style.color="darkblue"
+        document.getElementById("Seconds").style.color="white"  
+        
+        
+    }
+    else if(time>=16 && time<20)
+    {
+        document.getElementById("message").innerHTML="Good Evening!!" 
+        document.getElementById("vid").src="./evening.mp4" 
+        document.getElementById("container").style.backgroundColor="rgba(0, 0, 0, 0.604)"
+        document.getElementById("Time").style.color="white"
+        document.getElementById("Date").style.color="wheat"
+        document.getElementById("Day").style.color="wheat"
+        document.getElementById("Seconds").style.color="black"
+    }
+    else
+    {
+        document.getElementById("message").innerHTML="GoodNight!!" 
+        document.getElementById("vid").src="./night.mp4" 
+        document.getElementById("container").style.backgroundColor="rgba(0, 0, 0, 0.684)"
+        document.getElementById("Time").style.color="white"
+        document.getElementById("Date").style.color="wheat"
+        document.getElementById("Day").style.color="wheat"
+        document.getElementById("Seconds").style.color="black"
+    }   
+}
+greet()
+function removeTheGreetWindow(){
+    document.getElementById("greet").style.display="none"
+}
+setTimeout( removeTheGreetWindow,3000)
+
+function removeAlarm()
+{
+    document.getElementById("Alarm").style.display="none"
+}
+function loadAlarm()
+{
+    document.getElementById("Alarm").style.display="flex"
+}
+
+var audioFile=new Audio()
+audioFile.src="./audio.mp3"
+
+function setAlarm()
+{
+   var selec= document.querySelectorAll("select")
+   var userhh=selec[0].value
+   var usermm=selec[1].value
+   var useram_pm=selec[2].value
+   console.log(userhh,usermm,useram_pm)
+
+   var date=new Date()
+   var hh=date.getHours()
+   var mm=date.getMinutes()
+   var am_pm="AM"
+   if(hh>=12)
+   {
+    am_pm="PM"
+    if(hh>=13)
+    {
+        hh-=12;
+    }
+   }
+   if(hh==0)
+   {
+    hh=12
+   }
+   console.log(hh,mm,am_pm)
+   if(userhh==hh && usermm==mm && useram_pm==am_pm)
+   {
+    audioFile.play();
+   }
+   else
+   {
+    audioFile.pause();
+   }
+   setTimeout(setAlarm,1000)
+   document.getElementById("Alarm").style.display="none"
+   
+}
+
+
+
